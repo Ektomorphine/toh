072b419f1318de17d0325e435f7d8b9ea63c4848
GladkovM <iglad96@gmail.com>
2017-09-27 10:45:53 +0300

    Changes for stable work of root app with services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from './model-hero';
+import { HeroService } from './hero.service';
 
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
-
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
-
-  heroes : Hero[];
+  heroes: Hero[];
   selectedHero: Hero;
 
+  constructor(private heroService: HeroService) { }
 
-  onSelect(hero: Hero): void{
-      this.selectedHero = hero;
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+
 
 }
