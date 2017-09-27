60f047b37f4e88875d4b7cd07f9fd0d61ecaa451
GladkovM <iglad96@gmail.com>
2017-09-27 19:18:41 +0300

    Heroes component changes and add dashboard, hero-detail css

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Hero } from './model-hero';
 import { HeroService } from './hero.service';
+
 
 @Component({
   selector: 'my-heroes',
@@ -9,11 +11,13 @@ import { HeroService } from './hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  title = 'Tour of Heroes';
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private router: Router,
+    private heroService: HeroService) { }
+
 
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
@@ -26,6 +30,10 @@ export class HeroesComponent implements OnInit {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
+  gotoDetail(): void {
+  this.router.navigate(['/detail', this.selectedHero.id]);
+}
 
 
 
