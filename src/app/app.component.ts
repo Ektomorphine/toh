a771ae97a38a7c1596de97d023482c350f39ce4b
GladkovM <iglad96@gmail.com>
2017-09-27 16:05:26 +0300

    Add heroes component, changed root component for connecting  with heroes component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-
-import { Hero } from './model-hero';
-import { HeroService } from './hero.service';
-
-
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [HeroService]
+  selector: 'my-app',
+  templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Tour of Heroes';
-  heroes: Hero[];
-  selectedHero: Hero;
-
-  constructor(private heroService: HeroService) { }
-
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-
-
 }
